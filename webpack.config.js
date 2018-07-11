8632b315e1b3b3cbc58b754f63c1862f84065cd6
Taranveer Bains <taranveer786@gmail.com>
2018-07-10 21:19:32 -0700

    Add comment for sideConfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
 
+// if you go into the build-utils for webpack.server, you will see that what is returned from export
+// is a function and therefore, we call that function that returns an object that webpack can use
+// we pass along the side parameter because that's just good functional manners :)
 const sideConfig = side => require(`./build-utils/webpack.${side}`)(side);
 
 module.exports = ({ mode, side } = { mode: 'production', side: 'server' }) =>
