804d3b7ea8fcfee36ebd0b762f2fa1f9ca017e32
Taranveer Bains <taranveer786@gmail.com>
2018-07-25 14:32:24 -0700

    Add override for getAll for employee so password is omitted.

--- server/api/resources/employee/employee.controller.js
+++ server/api/resources/employee/employee.controller.js
@@ -1,4 +1,17 @@
 import generateControllers from '../../modules/controllers';
 import Employee from './employee.model';
 
-export default generateControllers(Employee);
+const getAll = model => (req, res, next) =>
+  model
+    .find({})
+    .select('-password')
+    .then(docs => {
+      res.json(docs);
+    })
+    .catch(err => next(err));
+
+const overWrite = {
+  getAll: getAll(Employee)
+};
+
+export default generateControllers(Employee, overWrite);
--- server/server.js
+++ server/server.js
@@ -2,14 +2,14 @@ import express from 'express';
 import connectToDb from './db';
 import setupMiddleware from './middleware';
 import restRouter from './api/restRouter';
-import { verifyUser, signIn } from './api/modules/auth';
+import protect, { verifyUser, signIn } from './api/modules/auth';
 
 const app = express();
 setupMiddleware(app);
 connectToDb();
 
 app.use('/signin', verifyUser, signIn);
-app.use('/api', restRouter);
+app.use('/api', protect, restRouter);
 
 app.all('*', (req, res) => {
   res.json({ ok: true });
