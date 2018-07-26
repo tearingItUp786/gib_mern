6eaeb6c64c2a2fe04a7305e10d5d492402920639
Taranveer Bains <taranveer786@gmail.com>
2018-07-25 17:50:00 -0700

    Add webpack client config for development

--- build-utils/webpack.client.js
+++ build-utils/webpack.client.js
@@ -1,3 +1,24 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = () => ({
-  entry: './client/index.js'
+  entry: './client/ClientApp.jsx',
+  devServer: {
+    hot: true,
+    port: 9000
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Custom template',
+      // Load a custom template (lodash by default see the FAQ for details)
+      template: './client/index.html'
+    })
+  ],
+  output: {
+    filename: 'client_bundle.js'
+  }
 });
---
+++ client/ClientApp.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App';
+
+const rootEl = document.getElementById('app');
+
+const renderApp = Component => {
+  render(<Component />, rootEl);
+};
+renderApp(App);
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextRootContainer = require('./App').default;
+    renderApp(NextRootContainer);
+  });
+}
