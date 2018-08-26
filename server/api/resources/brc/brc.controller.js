86db88f5789e9248b1717bdf55aa08771a4fd0be
Taran Bains <taranveer.bains@enginedigital.com>
2018-08-25 22:53:47 -0700

    Feature: Update BRC controller to handle sorting
    Fixes BH-#

--- server/api/resources/brc/brc.controller.js
+++ server/api/resources/brc/brc.controller.js
@@ -1,14 +1,86 @@
+import { mapKeys } from 'lodash';
 import brcModel from './brc.model';
 import volumeModel from '../volume/volume.model';
 import generateControllers from '../../modules/controllers';
 
+// function to turn queries into sortByObject
+const product_type = 'product_type';
+const product_name = 'product_name';
+const invoice = 'invoice';
+
+const createOrderBySortObject = query =>
+  Object.keys(query).map(objKey => {
+    switch (objKey) {
+      case 'productType':
+        return {
+          productType: query[objKey]
+        };
+      case 'name':
+        return {
+          'product.name': query[objKey]
+        };
+      default:
+        return {};
+    }
+  });
+
 const getAll = model => (req, res, next) => {
+  // create the order by objects that are needed
+  const orderBySorts = createOrderBySortObject(req.query);
+
+  // reduce the objects created by orderbysortobjects to be just one object
+  const reducedObjects = orderBySorts.reduce((accum, currVal) => {
+    const key = Object.keys(currVal)[0];
+    const value = Object.values(currVal)[0];
+    if (key && value) {
+      accum[key.toString()] = Number(value);
+      return accum;
+    }
+    return accum;
+  }, {});
+
   model
-    .find({})
-    .populate('invoice')
-    .populate('warehouse')
-    .populate('product')
-    .populate('productType')
+    .aggregate([
+      {
+        $lookup: {
+          from: 'products',
+          localField: 'product',
+          foreignField: '_id',
+          as: 'product'
+        }
+      },
+      { $unwind: '$product' },
+      {
+        $lookup: {
+          from: 'warehouses',
+          localField: 'warehouse',
+          foreignField: '_id',
+          as: 'warehouse'
+        }
+      },
+      { $unwind: '$warehouse' },
+      {
+        $lookup: {
+          from: 'invoices',
+          localField: 'invoice',
+          foreignField: '_id',
+          as: 'invoice'
+        }
+      },
+      { $unwind: '$invoice' },
+      {
+        $lookup: {
+          from: 'producttypes',
+          localField: 'productType',
+          foreignField: '_id',
+          as: 'productType'
+        }
+      },
+      {
+        $sort: reducedObjects
+      },
+      { $unwind: '$productType' }
+    ])
     .then(docs => res.status(201).json(docs))
     .catch(err => next(err));
 };
--- server/api/resources/product/product.controller.js
+++ server/api/resources/product/product.controller.js
@@ -1,4 +1,16 @@
 import generateControllers from '../../modules/controllers';
 import Product from './product.model';
 
-export default generateControllers(Product);
+const getAll = model => (req, res, next) => {
+  model
+    .find({})
+    .sort({ name: 'asc' })
+    .then(docs => res.status(201).json(docs))
+    .catch(err => next(err));
+};
+
+const overRides = {
+  getAll: getAll(Product)
+};
+
+export default generateControllers(Product, overRides);
